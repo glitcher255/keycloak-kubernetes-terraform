72da286852324746a42ed3de8b328a4a067ec569
Glitcher <testmail@mail.com>
2025-07-10 09:24:59 +0300

    pipeline-update

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,6 +3,12 @@ set -e
 # Always run from repo root
 cd "$(dirname "$0")/.."
 
+echo "[+] Installing Ansible collections..."
+ansible-galaxy collection install -r ansible/requirements.yml
+
+echo "[+] Installing Ansible K8s Python dependencies..."
+pipx inject ansible-core kubernetes openshift
+
 terraform apply -auto-approve
 terraform output -raw kube_config > kubeconfig/kubeconfig.yaml
 
