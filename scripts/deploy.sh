5ec9cfe262270d0adefecb3eedd3c7eb888ca9c9
Glitcher <testmail@mail.com>
2025-07-10 05:36:39 +0300

    pipeline

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,8 +8,7 @@ ansible-galaxy collection install -r ansible/requirements.yml
 
 echo "[+] Installing Ansible K8s Python dependencies..."
 python3 -m pip install --upgrade pip
-# pip install kubernetes openshift
-/opt/pipx/venvs/ansible-core/bin/pip install kubernetes openshift
+python3 -m pip install kubernetes openshift
 
 terraform apply -auto-approve
 terraform output -raw kube_config > kubeconfig/kubeconfig.yaml
